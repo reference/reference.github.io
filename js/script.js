736540163bb5fbbf73c4cb6780833b95b308e954
Ban <imti_bandianhong@126.com>
2020-12-11 15:01:51 +0800

    Site updated: 2020-12-11 15:01:50

--- js/script.js
+++ js/script.js
@@ -1,103 +1,88 @@
-(function ($) {
-  console.log('© Theme-Vexo | https://github.com/yanm1ng/hexo-theme-vexo')
-  var app = $('.app-body')
-  var header = $('.header')
-  var banner = document.getElementById('article-banner') || false
-  var about = document.getElementById('about-banner') || false
-  var top = $('.scroll-top')
-  var catalog = $('.catalog-container .toc-main')
-  var isOpen = false
+// declaraction of document.ready() function.
+(function () {
+    var ie = !!(window.attachEvent && !window.opera);
+    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
+    var fn = [];
+    var run = function () {
+        for (var i = 0; i < fn.length; i++) fn[i]();
+    };
+    var d = document;
+    d.ready = function (f) {
+        if (!ie && !wk && d.addEventListener)
+            return d.addEventListener('DOMContentLoaded', f, false);
+        if (fn.push(f) > 1) return;
+        if (ie)
+            (function () {
+                try {
+                    d.documentElement.doScroll('left');
+                    run();
+                } catch (err) {
+                    setTimeout(arguments.callee, 0);
+                }
+            })();
+        else if (wk)
+            var t = setInterval(function () {
+                if (/^(loaded|complete)$/.test(d.readyState))
+                    clearInterval(t), run();
+            }, 0);
+    };
+})();
 
-  $(document).ready(function () {
-    NProgress.start()
-    $('#nprogress .bar').css({
-      'background': '#42b983'
-    })
-    $('#nprogress .spinner').hide()
 
-    var fade = {
-      transform: 'translateY(0)',
-      opacity: 1
+document.ready(
+    // toggleTheme function.
+    // this script shouldn't be changed.
+    function () {
+        var _Blog = window._Blog || {};
+        const currentTheme = window.localStorage && window.localStorage.getItem('theme');
+        const isDark = currentTheme === 'dark';
+        if (isDark) {
+            document.getElementById("switch_default").checked = true;
+            // mobile
+            document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+        } else {
+            document.getElementById("switch_default").checked = false;
+            // mobile
+            document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+        }
+        _Blog.toggleTheme = function () {
+            if (isDark) {
+                document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                // mobile
+                document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+            } else {
+                document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                // mobile
+                document.getElementById("mobile-toggle-theme").innerText = "· Light"
+            }
+            document.getElementsByClassName('toggleBtn')[0].addEventListener('click', () => {
+                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
+                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                } else {
+                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                }
+                window.localStorage &&
+                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light',)
+            })
+            // moblie
+            document.getElementById('mobile-toggle-theme').addEventListener('click', () => {
+                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
+                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                    // mobile
+                    document.getElementById("mobile-toggle-theme").innerText = "· Light"
+
+                } else {
+                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                    // mobile
+                    document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+                }
+                window.localStorage &&
+                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light',)
+            })
+        };
+        _Blog.toggleTheme();
+
+        // ready function.
+
     }
-    if (banner) {
-      app.css('transition-delay', '0.15s')
-      $('#article-banner').children().css(fade)
-    }
-    if (about) {
-      $('.author').children().css(fade)
-    }
-    app.css(fade)
-  })
-
-  window.onload = function () {
-    setTimeout(function () {
-      NProgress.done()
-    }, 200)
-  }
-
-  $('.menu').on('click', function () {
-    if (!header.hasClass('fixed-header') || isOpen) {
-      header.toggleClass('fixed-header')
-      isOpen = !isOpen
-    }
-    $('.menu-mask').toggleClass('open')
-  })
-
-  $('#tag-cloud a').on('click', function () {
-    var list = $('.tag-list')
-    var name = $(this).data('name')
-    var maoH = list.find('#' + name).offset().top
-
-    $('html,body').animate({
-      scrollTop: maoH - header.height()
-    }, 500)
-  })
-
-  $('.reward-btn').on('click', function () {
-    $('.money-code').fadeToggle()
-  })
-
-  $('.arrow-down').on('click', function () {
-    $('html, body').animate({
-      scrollTop: banner.offsetHeight - header.height()
-    }, 500)
-  })
-
-  $('.toc-nav a').on('click', function (e) {
-    e.preventDefault()
-    var catalogTarget = e.currentTarget
-    var scrollTarget = $(catalogTarget.getAttribute('href'))
-    var top = scrollTarget.offset().top
-    if (top > 0) {
-      $('html,body').animate({
-        scrollTop: top - 65
-      }, 500)
-    }
-  })
-
-  top.on('click', function () {
-    $('html, body').animate({ scrollTop: 0 }, 600)
-  })
-
-  document.addEventListener('scroll', function () {
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
-    var headerH = header.height()
-    if (banner) {
-      if (scrollTop > headerH) {
-        header.addClass('fixed-header')
-      } else if (scrollTop === 0) {
-        header.removeClass('fixed-header')
-      }
-    }
-    if (scrollTop > 100) {
-      top.addClass('opacity')
-    } else {
-      top.removeClass('opacity')
-    }
-    if (scrollTop > 190) {
-      catalog.addClass('fixed-toc')
-    } else {
-      catalog.removeClass('fixed-toc')
-    }
-  })
-})(jQuery)
+);
